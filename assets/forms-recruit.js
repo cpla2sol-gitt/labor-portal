8aacf80be8b5831940582d8e85c548ec2b0af12e
cpla2sol-gitt <cpla.2sol@gmail.com>
2026-05-14 09:21:02 +0900

    Update forms-recruit.js

--- assets/forms-recruit.js
+++ assets/forms-recruit.js
@@ -75,7 +75,7 @@ roster: {
           <td class="lbl">퇴직일</td><td class="val"><input class="di"/></td></tr>
       <tr><td class="lbl">퇴직사유</td><td class="val"><input class="di"/></td>
           <td class="lbl">금품청산</td><td class="val"><input class="di"/></td></tr>
-      <tr><td class="lbl">특기사항</td><td class="val" colspan="3"><textarea class="di" placeholder="교육, 건강, 휴직 등" style="min-height:120pt"></textarea></td></tr>
+      <tr><td class="lbl">특기사항</td><td class="val" colspan="3"><textarea class="di" placeholder="교육, 건강, 휴직 등" style="min-height:220pt"></textarea></td></tr>
     </table>`
 },
 
