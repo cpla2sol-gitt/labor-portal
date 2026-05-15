ba436bc855c940d15c2f5fc1f2811e6a3a53aa4a
cpla2sol-gitt <cpla.2sol@gmail.com>
2026-05-15 17:41:39 +0900

    Update forms-recruit.js

--- assets/forms-recruit.js
+++ assets/forms-recruit.js
@@ -11,7 +11,7 @@ privacy: {
   html: () => `
     <div class="doc-title">개인정보의 수집·이용에 관한 동의서</div>
 
-    <div class="body-text" style="font-size:10pt;margin-bottom:5pt">
+    <div class="body-text" style="font-size:10pt;margin-bottom:3pt">
       <input class="ii" placeholder="홍길동" style="width:60pt"/>은(는) <strong><input class="ii c-company" style="width:120pt"/></strong>의 재직근로자로서,
       인사관리상 개인정보의 수집·이용이 필요하다는 것을 이해하고 있고 다음과 같은 개인정보·민감정보·고유식별정보를 수집·이용하는 것에 동의합니다.
     </div>
@@ -28,7 +28,7 @@ privacy: {
         </tr>
       </tbody>
     </table>
-    <div class="body-text" style="font-size:9.5pt;text-align:right;margin-bottom:3pt">
+    <div class="body-text" style="font-size:9.5pt;text-align:right;margin-bottom:1pt">
       개인정보의 수집·이용에(□동의함 □동의하지 않음)
     </div>
 
@@ -44,7 +44,7 @@ privacy: {
         </tr>
       </tbody>
     </table>
-    <div class="body-text" style="font-size:9.5pt;text-align:right;margin-bottom:3pt">
+    <div class="body-text" style="font-size:9.5pt;text-align:right;margin-bottom:1pt">
       민감정보의 수집·이용에(□동의함 □동의하지 않음)
     </div>
 
@@ -60,15 +60,15 @@ privacy: {
         </tr>
       </tbody>
     </table>
-    <div class="body-text" style="font-size:9.5pt;text-align:right;margin-bottom:3pt">
+    <div class="body-text" style="font-size:9.5pt;text-align:right;margin-bottom:1pt">
       고유식별정보의 수집·이용에(□동의함 □동의하지 않음)
     </div>
       
-    <div class="body-text" style="font-size:10pt;margin-bottom:6pt">
+    <div class="body-text" style="font-size:10pt;margin-bottom:3pt">
       본사는 취득한 개인정보를 수집한 목적에 필요한 범위에서 적합하게 처리하고 그 목적 외의 용도로 사용하지 않으며 개인 정보를 제공한 계약당사자는 언제나 자신이 입력한 개인정보를 열람수정 및 정보제공에 대한 철회를 할 수 있습니다. 본인은 1~3항에 따라 수집되는 개인정보의 항목과 개인정보의 수집·이용에 대한 거부를 할 수 있는 권리가 있다는 사실을 충분히 설명 받고 숙지하였으며, 미동의시 적법하게 시행되는 회사내부규정 및 법령에 따라 발생하는 불이익에 대한 책임은 본인에게 있음을 확인합니다.
     </div>
 
-    <div style="margin-top:16pt;text-align:center;font-size:10pt">
+    <div style="margin-top:5pt;text-align:center;font-size:10pt">
       <input class="ii" placeholder="20" style="width:30pt"/>년 <input class="ii" placeholder="01" style="width:22pt"/>월 <input class="ii" placeholder="01" style="width:22pt"/>일
     </div>
     <div style="display:flex;justify-content:flex-end;margin-top:10pt;flex-shrink:0">
